08a95d17a2587a70128dc822562a021e674b875b
Manas Dubey <147379602+vikingmanas@users.noreply.github.com>
2026-05-14 10:04:43 +0530

    Initial Commit

---
+++ LoanManagementSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoanManagementSystem
+//
+//  Created by apple on 14/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
